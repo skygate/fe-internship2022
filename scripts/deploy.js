478b00cbf15498f388349ea85b4cbb8f576b018e
Jakub Szymański <54314608+JSzymanskiJS@users.noreply.github.com>
2022-02-21 20:45:28 +0100

    Changed deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,22 +1,22 @@
 const { network, ethers } = require("hardhat");
-const { helperData } = require("helper-hardhat-config.js")
+// const { helperData } = require("helper-hardhat-config.js"); //This line is useful when we will be using different blockchains.
 
 async function main() {
+  const DECIMALS = '18'
+  const INITIAL_PRICE = '200000000000000000000'
+
   const [deployer] = await ethers.getSigners();
-  const [networkName] = ethers.networkName
-
   console.log("Deploying contracts with the account:", deployer.address);
-
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
-
   const MockV3Aggregator = await ethers.getContractFactory("MockV3Aggregator");
-  mockV3Aggregator = await MockV3Aggregator.deploy(DECIMALS, INITIAL_PRICE,);
+  mockV3Aggregator = await MockV3Aggregator.deploy(DECIMALS, INITIAL_PRICE);
   await mockV3Aggregator.deployed();
+  console.log("MockV3Aggregator address:", mockV3Aggregator.address);
+
 
   const BaseERC721 = await ethers.getContractFactory("BaseERC721");
   const baseERC721 = await BaseERC721.deploy("My base ERC721", "Base ERC721", mockV3Aggregator.address);
-
   console.log("BaseERC721 address:", baseERC721.address);
 }
 
