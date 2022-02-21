b5b869a781f4dc721c7aa4061fb20eed4778e2eb
Jakub Szymański <54314608+JSzymanskiJS@users.noreply.github.com>
2022-02-21 12:01:09 +0100

    2nd try to commit changes

--- frontend/src/components/startSale/StartSale.jsx
+++ frontend/src/components/startSale/StartSale.jsx
@@ -5,13 +5,13 @@ import { ethers } from 'ethers';
 
 const StartSale = (props) => {
     const startSale = async () => {
-        if(props.activeAccountProps) {
+        if (props.activeAccountProps) {
             const [address, provider, signer, contract] = getContractComponents();
 
             const tokenId = document.getElementById('sellTokenId').value;
             const price = ethers.utils.parseEther(document.getElementById('sellTokenPrice').value);
             await contract.startSale(tokenId, price)
-                .then(() => { console.log(`>>> Token ${tokenId} has been put on sale for ${price/10**18} ETH!`); })
+                .then(() => { console.log(`>>> Token ${tokenId} has been put on sale for ${price / 10 ** 18} ETH!`); })
                 .catch((error) => {
                     console.log(error.data.message);
                 })
