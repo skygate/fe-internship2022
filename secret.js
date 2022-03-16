6c276dec4777d4d6def5619fb2e213c58fe50f1d
Amit Dua <80542014+duaamit@users.noreply.github.com>
2022-03-16 16:33:14 +0100

    Delete secret.js

--- secret.js
+++
@@ -1,6 +0,0 @@
-module.exports={
-    ALCHEMY_ROPSTEN_API_KEY: "https://eth-ropsten.alchemyapi.io/v2/WWZfD89Tt7tWD_lD4OkcKC_B4r1kAn2N",
-    ROPSTEN_PRIVATE_KEY: "9f0593f55edf5efd770092129cdfca54c56dbeb578f7cba9a85773bcf38de961",
-    ALCHEMY_RINKEBY_API_KEY: "https://eth-rinkeby.alchemyapi.io/v2/IAdlQnGO-3bGgAbSArT7CBOovHYQwCaL",
-    RINKEBY_PRIVATE_KEY: "9f0593f55edf5efd770092129cdfca54c56dbeb578f7cba9a85773bcf38de961"
-}
