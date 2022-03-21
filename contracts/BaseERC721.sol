f168728ee955d1f5599f4aeb6d523f3e37364db6
kornelHub <67650984+kornelHub@users.noreply.github.com>
2022-03-21 09:56:24 +0100

    [NMI-55] Delete comment

--- contracts/BaseERC721.sol
+++ contracts/BaseERC721.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
-// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
