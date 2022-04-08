3c599f4864c659db172144243b7d7133891aa1a6
Dorota Nowak <74257254+dorotanowak00@users.noreply.github.com>
2022-04-08 13:22:02 +0200

    Nmi 79 creator network (#65)
    * added new section Creator Network
    
    * Finished creator network component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import "./App.css";
 import { Footer } from "./components";
-import { Banner } from "./components";
-import { Navbar } from "./components";
+
+import { Navbar } from './components';
+import { Banner } from './components';
+import { CreatorNetwork } from './components';
 import { TopBrandsView } from "./views";
 
 function App() {
     return (
         <div className="App">
             <Navbar />
-            <Banner />
+            <Banner />      
+            <CreatorNetwork />
             <TopBrandsView />
             <Footer />
         </div>
--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,6 +1,7 @@
 export { Footer } from "./footer/Footer";
-export { Navbar } from "./navbar/Navbar";
-export { Banner } from "./banner/Banner";
+export { Navbar } from './navbar/Navbar';
+export { Banner } from './banner/Banner';
+export { CreatorNetwork } from './creatorNetwork/CreatorNetwork';
 export { MainNFT } from "./topBrands/MainNFT/MainNFT";
 export { NFTList } from "./topBrands/NFTList/NFTList";
 export { CreatorsList } from "./topBrands/CreatorsList/CreatorsList";
