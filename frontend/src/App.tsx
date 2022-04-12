f2b15656bcd80cb978f464cab768c64193f02bee
Dorota Nowak <74257254+dorotanowak00@users.noreply.github.com>
2022-04-12 14:20:42 +0200

    Nmi 83 home page popular (#73)
    * added Popular section with filters
    
    * added PopularItem component
    
    * aded PopularItem Component
    
    * fixes after rewiev
    
    * resolved conflixts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import { Navbar } from './components';
 import { Banner } from './components';
 import { CreatorNetwork } from './components';
 import { TopBrandsView } from "./views";
+import { Popular } from './components';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Banner />      
           <CreatorNetwork />
           <TopBrandsView />
+          <Popular />
           <Footer />
       </div>
   );
