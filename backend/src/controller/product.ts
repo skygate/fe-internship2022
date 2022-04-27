b21b48eaf14de2e7cd5eac9fac7e9d763ae6c38c
dorotanowak00 <dorota.nowak00@gmail.com>
2022-04-27 11:51:55 +0200

    added basic get methods to Products Routes

--- backend/src/controller/product.ts
+++ backend/src/controller/product.ts
@@ -1,6 +1,40 @@
 import { Request, Response } from 'express';
-import { ExampleNFTList } from '../constants/ExampleNFTList';
+
+const productsArray = new Array(20);
+
+interface Product {
+	productId: string;
+	productName: string;
+	productDescription: string;
+	productImageUrl: string;
+	productCategory: string;
+}
+
+const createProductsArray = (arr: Product[]) => {
+	for (let i = 0; i < arr.length; i++) {
+		arr[i] = {
+			productId: `productId${i}`,
+			productName: `exampleName${i}`,
+			productDescription:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+			productImageUrl: `https://picsum.photos/id/${Math.round(
+				Math.random() * 100
+			)}/200`,
+			productCategory: 'category',
+		};
+	}
+};
+
+createProductsArray(productsArray);
 
 module.exports.getAllProducts = (req: Request, res: Response) => {
-	res.json(ExampleNFTList);
+	res.json(productsArray);
+};
+
+module.exports.getProduct = (req: Request, res: Response) => {
+	const id: number = Number(req.params.id);
+
+	productsArray.find((item) => item.productId === `productId${id}`)
+		? res.json(productsArray[id])
+		: res.send('Nie ma takiego produktu');
 };
--- backend/src/routes/product.ts
+++ backend/src/routes/product.ts
@@ -3,5 +3,6 @@ const router = express.Router();
 const product = require('../controller/product');
 
 router.get('/', product.getAllProducts);
+router.get('/:id', product.getProduct);
 
 module.exports = router;
