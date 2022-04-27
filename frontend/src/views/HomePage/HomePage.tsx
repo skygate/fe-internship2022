4b415bfdb8a591cbbd3285e28d6980a79350aa44
dorotanowak00 <dorota.nowak00@gmail.com>
2022-04-27 15:40:51 +0200

    updated Discover with endpoint

--- frontend/src/views/HomePage/HomePage.tsx
+++ frontend/src/views/HomePage/HomePage.tsx
@@ -7,14 +7,8 @@ import { HotBid } from './';
 import { HotCollections } from './';
 
 export const HomePage = () => {
-	const showData = () => {
-		fetch('http://localhost:8000/nfts')
-			.then((response) => response.json())
-			.then((res) => console.log(res));
-	};
 	return (
 		<>
-			{showData()}
 			<Banner />
 			<CreatorNetwork />
 			<TopBrandsView />
