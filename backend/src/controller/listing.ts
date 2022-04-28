a939d2bac95a5958198898f3d0eac67040c79933
Puzjeh <puzmarcin@gmail.com>
2022-04-28 12:01:28 +0200

    added post, patch, delete endpoints for listing

--- backend/src/controller/listing.ts
+++ backend/src/controller/listing.ts
@@ -3,25 +3,32 @@ import ExampleListingList from "../constants/ExampleListingList";
 import ExampleUserList from "../constants/ExampleUserList";
 import productsArray from "../controller/product";
 
-const fullInfoListings = new Array();
-ExampleListingList.map((listing) => {
-  const listingItem = productsArray.find(
-    (product) => product.productId === listing.productID
-  );
-  fullInfoListings.push({
-    listingID: listing.listingID,
-    price: listing.price,
-    amount: listing.amount,
-    highestBid: listing.highestBid,
-    startDate: listing.startDate,
-    endDate: listing.endDate,
-    likes: listing.likes,
-    productImageUrl: listingItem.productImageUrl,
-    productName: listingItem.productName,
-    productDescription: listingItem.productDescription,
-    productID: listingItem.productId,
+const listingList = ExampleListingList;
+
+let fullInfoListings = new Array();
+function fillFullInfo() {
+  fullInfoListings = new Array();
+  listingList.map((listing) => {
+    const listingItem = productsArray.find(
+      (product) => product.productId === listing.productID
+    );
+    fullInfoListings.push({
+      listingID: listing.listingID,
+      userID: listing.userID,
+      price: listing.price,
+      amount: listing.amount,
+      highestBid: listing.highestBid,
+      startDate: listing.startDate,
+      endDate: listing.endDate,
+      likes: listing.likes,
+      productImageUrl: listingItem.productImageUrl,
+      productName: listingItem.productName,
+      productDescription: listingItem.productDescription,
+      productID: listingItem.productId,
+    });
   });
-});
+}
+fillFullInfo();
 
 module.exports.getAllListings = (req: Request, res: Response) => {
   res.json(ExampleListingList);
@@ -35,14 +42,106 @@ module.exports.getListing = (req: Request, res: Response) => {
 };
 
 module.exports.getFullInfoListings = (req: Request, res: Response) => {
+  fillFullInfo();
   res.json(fullInfoListings);
 };
 
 module.exports.getFullInfoListing = (req: Request, res: Response) => {
+  fillFullInfo();
   const fullInfoListing = fullInfoListings.find(
     (Listing) => Listing.listingID === req.params.id
   );
   fullInfoListing
     ? res.json(fullInfoListing)
-    : res.send("listing doesn`t exist");
+    : res.status(404).json({ errorMessage: "Rosources not found" });
+};
+
+module.exports.addListing = (req: Request, res: Response) => {
+  if (typeof req.body == undefined) {
+    res.json({
+      status: "error",
+      message: "data is undefined",
+    });
+  } else {
+    if (
+      req.body.userID &&
+      req.body.productID &&
+      req.body.amount &&
+      req.body.duration
+    ) {
+      const listing = {
+        listingID: `listing_num_${Math.random() * 999999999999999}`,
+        userID: req.body.userID,
+        productID: req.body.productID,
+        price: req.body.price,
+        amount: req.body.amount,
+        highestBid: 0,
+        startDate: new Date(),
+        endDate: new Date(
+          new Date().setHours(new Date().getHours() + req.body.duration)
+        ),
+        likes: 0,
+      };
+      listingList.push(listing);
+      fillFullInfo();
+    } else {
+      res.json({
+        status: "error",
+        message: "data has missing property",
+      });
+    }
+  }
+};
+
+module.exports.deleteListing = (req: Request, res: Response) => {
+  if (typeof req.body == undefined) {
+    res.json({
+      status: "error",
+      message: "data is undefined",
+    });
+  } else {
+    let index = listingList
+      .map(function (e) {
+        return e.listingID;
+      })
+      .indexOf(req.body.listingID);
+
+    index > -1
+      ? listingList.splice(index, 1)
+      : res.status(404).json({ errorMessage: "Rosources not found" });
+    fillFullInfo();
+  }
+};
+
+module.exports.editListing = (req: Request, res: Response) => {
+  if (typeof req.body == undefined) {
+    res.json({
+      status: "error",
+      message: "something went wrong! check your sent data",
+    });
+  } else {
+    let index = listingList
+      .map(function (e) {
+        return e.listingID;
+      })
+      .indexOf(req.body.listingID);
+
+    if (index > -1) {
+      req.body.price
+        ? (listingList[index].price = req.body.price)
+        : (listingList[index].price = listingList[index].price);
+      req.body.amount
+        ? (listingList[index].amount = req.body.amount)
+        : (listingList[index].amount = listingList[index].amount);
+      req.body.longerDuration
+        ? (listingList[index].endDate = new Date(
+            new Date().setHours(
+              listingList[index].endDate.getHours() + req.body.longerDuration
+            )
+          ))
+        : (listingList[index].endDate = listingList[index].endDate);
+    } else {
+      res.status(404).json({ errorMessage: "Rosources not found" });
+    }
+  }
 };
