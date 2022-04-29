17ca7177bc2e2b2fcb1128aa6c76863088866373
dorotanowak00 <dorota.nowak00@gmail.com>
2022-04-29 11:23:29 +0200

    create form fix

---
+++ frontend/src/components/types/createFormState.ts
@@ -0,0 +1,12 @@
+export interface createFormState {
+    productId: null;
+    productName: string;
+    productDescription: string;
+    productImageUrl: string;
+    productCategory: string;
+    productSize: string;
+    productProperties: string;
+    putOnSale: boolean;
+    instantSellPrice: boolean;
+    unlockOncePurchased: boolean;
+}
