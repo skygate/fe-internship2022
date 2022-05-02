2a45ddec763b5ce2e0f642d7e07bc9315118dcd6
dorotanowak00 <dorota.nowak00@gmail.com>
2022-05-02 08:42:38 +0200

    add new branch

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,13 +10,16 @@ const port = process.env.PORT;
 const productRoute = require("./routes/product");
 const userRoute = require("./routes/user");
 const listingRoute = require("./routes/listing");
+const loginRoute = require("./routes/login");
+const registerROute = require("./routes/register");
 
 app.use(cors());
 app.use(express.json());
 app.use("/products", productRoute);
 app.use("/user", userRoute);
 app.use("/listing", listingRoute);
+app.use("/login", listingRoute);
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+    console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
 });
