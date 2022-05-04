d1422feda08e88a4aef684cf018bb31f4c8472b6
dorotanowak00 <dorota.nowak00@gmail.com>
2022-05-04 15:32:36 +0200

    added form validation

--- backend/src/controller/user.ts
+++ backend/src/controller/user.ts
@@ -1,69 +1,90 @@
 import { Request, Response } from "express";
-// import ExampleUserList from "../constants/ExampleUserList";
+import { body, validationResult } from "express-validator";
 import bcrypt from "bcrypt";
 import passport from "passport";
+import user from "../models/users";
 const initializePassport = require("../passport-config");
 
 const usersArray = new Array(1);
 
 interface User {
-    userID: number;
     username: string;
     email: string;
     password: string;
-    // about: string;
-    // websiteUrl: string;
-    // profilePicture: string;
-    // coverPicture: string;
 }
 
-const createUsersArray = async (arr: User[]) => {
-    for (let i = 0; i < arr.length; i++) {
-        arr[i] = {
-            userID: i,
-            username: `ExampleUser${i}`,
-            email: `e${i}@e`,
-            password: await bcrypt.hash(`password${i}`, 10),
-        };
-    }
-};
+//FUNKCJA DO GENEROWANIA BAZY USERÓW
 
-createUsersArray(usersArray);
+// const createUsersArray = async () => {
+//     for (let i = 0; i < 20; i++) {
+//         const newUser = new user({
+//             userID: Date.now().toString() + i,
+//             username: `ExampleUser${i}`,
+//             email: `mail${i}@mail.pl`,
+//             password: await bcrypt.hash(`password${i}`, 10),
+//         });
+
+//         newUser.save();
+//     }
+// };
+
+// createUsersArray();
 
 initializePassport(
     passport,
-    (email: string) => usersArray.find((user) => user.email === email),
-    (id: number) => usersArray.find((user) => user.userID === id)
+    (email: string) => user.findOne({ email: email }),
+    (id: string) => user.findById(id)
 );
 
-module.exports.getAllUsers = (req: Request, res: Response) => {
-    res.json(usersArray);
+module.exports.getAllUsers = async (req: Request, res: Response) => {
+    try {
+        const users = await user.find();
+        res.status(200).json(users);
+    } catch (error: any) {
+        res.status(404).json({ message: error.message });
+    }
 };
 
-module.exports.getUser = (req: Request, res: Response) => {
-    const user = usersArray.find((User) => User.userID == req.params.id);
-    user ? res.json(user) : res.send("user doesn`t exist");
+module.exports.getUser = async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    try {
+        const foundUser = await user.findById(id);
+        res.status(200).json(foundUser);
+    } catch (error: any) {
+        res.status(404).json({ message: error.message });
+    }
 };
 
 module.exports.registerUser = async (req: Request, res: Response) => {
-    const existingUser = usersArray.find((user: User) => user.email == req.body.email);
+    const { email, username, password } = req.body;
 
-    if (existingUser != null) {
-        res.send("User already exists");
+    const existingEmail = await user.findOne({ email: email });
+    const existingUsername = await user.findOne({ username: username });
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    if (existingEmail != null || existingUsername != null) {
+        res.send(`User already exists`);
         return;
     }
 
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new user({
+        userID: Date.now().toString(),
+        username: username,
+        email: email,
+        password: hashedPassword,
+    });
+
     try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        usersArray.push({
-            userID: Date.now().toString(),
-            username: req.body.username,
-            email: req.body.email,
-            password: hashedPassword,
-        });
-        res.send("udalo sie");
-    } catch {
-        res.send("nie udało się");
+        newUser.save();
+        res.status(201).json(newUser);
+    } catch (error: any) {
+        res.status(409).json({ message: error.message });
     }
 };
 
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,10 +1,17 @@
 import express, { Request, Response } from "express";
+import { check, body, validationResult } from "express-validator";
 const router = express.Router();
 const user = require("../controller/user");
 
 router.get("/", user.getAllUsers);
 router.get("/:id", user.getUser);
-router.post("/register", user.registerUser);
+router.post(
+    "/register",
+    body("email").notEmpty().isEmail(),
+    body("password").notEmpty().isLength({ min: 3 }),
+    body("username").notEmpty().isLength({ min: 3 }),
+    user.registerUser
+);
 router.post("/login", user.loginUser);
 
 module.exports = router;
