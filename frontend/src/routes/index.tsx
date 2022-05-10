7b47db101f2b556088314359dfcca3e51a493844
Puzjeh <puzmarcin@gmail.com>
2022-05-10 15:10:53 +0200

    created view for profiles

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,19 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Footer, Navbar } from "components";
-import { LoginPage, HomePage, CreateSingleCollectible } from "views";
+import { LoginPage, HomePage, CreateSingleCollectible, Profile } from "views";
 
 function Router() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />}></Route>
-        <Route path="/signin" element={<LoginPage />}></Route>
-        <Route path="/create" element={<CreateSingleCollectible />}></Route>
-      </Routes>
-      <Footer />
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<HomePage />}></Route>
+                <Route path="/signin" element={<LoginPage />}></Route>
+                <Route path="/create" element={<CreateSingleCollectible />}></Route>
+                <Route path="/profile/:profileID" element={<Profile />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+    );
 }
 
 export default Router;
--- frontend/src/views/index.ts
+++ frontend/src/views/index.ts
@@ -1,3 +1,4 @@
 export { HomePage } from "./HomePage/HomePage";
 export { LoginPage } from "./loginPage/LoginPage";
 export { CreateSingleCollectible } from "./CreateSingleCollectible/CreateSingleCollectible";
+export { Profile } from "./Profile/Profile";
