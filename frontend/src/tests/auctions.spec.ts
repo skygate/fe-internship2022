7bad08a632a35d00b37b842166a450ce425d67e6
Kamil <9657192+kkolasinski@users.noreply.github.com>
2022-06-02 17:31:45 +0200

    if paths

---
+++ frontend/src/tests/auctions.spec.ts
@@ -0,0 +1,43 @@
+import { store } from "store/store";
+import { getAuctions } from "store/auctions";
+
+const mockFetchAuctions = jest.fn();
+const mockFetchFilteredAndSortedAuctions = jest.fn();
+
+jest.mock("store/helpers/auctionsHelper", () => ({
+    fetchAuctions: () => mockFetchAuctions(),
+    fetchFilteredAndSortedAuctions(...args: unknown[]) {
+        return mockFetchFilteredAndSortedAuctions(...args);
+    },
+}));
+
+const initialState = {
+    status: "",
+    auctions: [],
+};
+
+const fakeAuctions = [{ id: 1 }, { id: 2 }];
+
+const getState = () => store.getState().auctions;
+
+describe("store/auctions.ts", () => {
+    it("should set initial state", () => {
+        expect(getState()).toStrictEqual(initialState);
+    });
+
+    it("should set unfiltered and unsorted auctions when flag is false", async () => {
+        mockFetchAuctions.mockResolvedValueOnce(Promise.resolve({ data: [...fakeAuctions] }));
+        await store.dispatch(getAuctions(false));
+        expect(mockFetchAuctions).toHaveBeenCalled();
+        expect(getState()).toStrictEqual({ status: "success", auctions: [...fakeAuctions] });
+    });
+
+    it("should set filtered and sorted auctions when flag is true", async () => {
+        mockFetchFilteredAndSortedAuctions.mockResolvedValueOnce(
+            Promise.resolve({ data: [...fakeAuctions] })
+        );
+        await store.dispatch(getAuctions(true));
+        expect(mockFetchFilteredAndSortedAuctions).toHaveBeenCalledWith(true);
+        expect(getState()).toStrictEqual({ status: "success", auctions: [...fakeAuctions] });
+    });
+});
