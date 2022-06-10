63393ca9a837783995f92cd917b1f56d198f4277
Dorota Nowak <74257254+dorotanowak00@users.noreply.github.com>
2022-06-10 15:29:04 +0200

    updated section HotBid (#51)
    * updated section HotBid
    
    * review fix

--- frontend/src/views/HomePage/HotBid/HotBid.tsx
+++ frontend/src/views/HomePage/HotBid/HotBid.tsx
@@ -1,27 +1,68 @@
-import React from "react";
-import { NFTProductCard } from "components";
-import { ExampleNFTList } from "constant/ExampleNFTList";
 import styles from "./HotBid.module.scss";
 import arrowLeft from "assets/arrowLeft.svg";
 import arrowRight from "assets/arrowRight.svg";
+import { useAppSelector } from "store/store";
+import { ProductCard } from "components";
+import { useEffect, useState } from "react";
+
+const AUCTIONS_PER_PAGE = 4;
 
 export function HotBid() {
+    const auctions = useAppSelector((state) => state.auctions.auctions);
+    const sortedAuctions = [...auctions];
+    sortedAuctions.sort((a, b) => b.bidHistory.length - a.bidHistory.length);
+    const LAST_PAGE_INDEX = Math.ceil(auctions.length / AUCTIONS_PER_PAGE) - 1;
+
+    const [activePage, setActivePage] = useState(0);
+    const [visibleAuctions, setVisibleAuctions] = useState(sortedAuctions.slice(0, 4));
+
+    const onPageChange = (arg: string) => {
+        if (arg === "prev" && activePage === 0) return;
+        if (arg === "next" && activePage === LAST_PAGE_INDEX) return;
+        if (arg === "prev") setActivePage((page) => page - 1);
+        if (arg === "next") setActivePage((page) => page + 1);
+    };
+
+    useEffect(() => {
+        setVisibleAuctions(sortedAuctions.slice(0, 4));
+    }, [auctions]);
+
+    useEffect(() => {
+        setVisibleAuctions(
+            sortedAuctions.slice(activePage * AUCTIONS_PER_PAGE, activePage + AUCTIONS_PER_PAGE)
+        );
+    }, [activePage]);
+
     return (
         <div className={styles.hotBidContainer}>
             <div className={styles.headerSection}>
                 <span>Hot bid</span>
                 <div className={styles.arrowButtons}>
-                    <button className={styles.arrowButton}>
+                    <button
+                        className={
+                            activePage === 0
+                                ? styles.arrowButton
+                                : `${styles.arrowButton} ${styles.hoverButton}`
+                        }
+                        onClick={() => onPageChange("prev")}
+                    >
                         <img src={arrowLeft} alt="left arrow" />
                     </button>
-                    <button className={styles.arrowButton}>
+                    <button
+                        className={
+                            activePage === LAST_PAGE_INDEX
+                                ? styles.arrowButton
+                                : `${styles.arrowButton} ${styles.hoverButton}`
+                        }
+                        onClick={() => onPageChange("next")}
+                    >
                         <img src={arrowRight} alt="right arrow" />
                     </button>
                 </div>
             </div>
             <div className={styles.nftsContainer}>
-                {ExampleNFTList.slice(0, 4).map((author) => (
-                    <NFTProductCard key={author.authorName} nft={author} />
+                {visibleAuctions.map((auction) => (
+                    <ProductCard key={auction._id} item={auction} />
                 ))}
             </div>
         </div>
