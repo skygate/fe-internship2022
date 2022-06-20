fba24c9d4dbc8c77b524b0d64183b53f3c009b65
Dorota Nowak <74257254+dorotanowak00@users.noreply.github.com>
2022-06-20 09:08:38 +0200

    Fei2022 70 creatornetwork update (#52)
    * added basic data and counter
    
    * added bidding modal and socket
    
    * quick fix

--- frontend/src/views/HomePage/creatorNetwork/CreatorNetwork.tsx
+++ frontend/src/views/HomePage/creatorNetwork/CreatorNetwork.tsx
@@ -1,67 +1,155 @@
-import React from "react";
-import homeImage from "assets/homeImage.png";
+import React, { useContext, useEffect, useState } from "react";
 import arrowLeft from "assets/arrowLeft.svg";
 import arrowRight from "assets/arrowRight.svg";
 import style from "./creatorNetwork.module.scss";
-import profilePicture from "assets/profilePicture.png";
 import exampleImage from "assets/exampleImage.png";
-import { ProfileHorizontal } from "components";
+import { ErrorToast, Modal, ProfileHorizontal, LoadingToast, UpdateToast } from "components";
+import { useAppSelector } from "store/store";
+import { AuctionItem } from "interfaces";
+import { Link } from "react-router-dom";
+import { intervalToDuration } from "date-fns";
+import { AddBidModal } from "components/Modal";
+import { addBid } from "API/UserService";
+import { BidOffer } from "interfaces";
+import { SocketContext } from "App";
+import { getAuction } from "API/UserService";
+import { UserSelector } from "store/user";
 
 export const CreatorNetwork = () => {
+    const socket = useContext(SocketContext);
+    const [auction, setAuction] = useState<AuctionItem>();
+    const [timeUntillAuctionEnds, setTimeUntillAuctionEnds] = useState<Duration>();
+    const [bidModalVisibility, setBidModalVisibility] = useState(false);
+    const lastBid = auction?.bidHistory[auction.bidHistory.length - 1]?.bid;
+    const user = useAppSelector(UserSelector);
+
+    useEffect(() => {
+        getAuction("62a07222a24e1df13264c9d0").then((res) => setAuction(res.data));
+    }, []);
+
+    useEffect(() => {
+        if (!auction) return;
+        const clockInterval = setInterval(() => {
+            const duration = intervalToDuration({
+                start: new Date(auction.endDate),
+                end: new Date(),
+            });
+            setTimeUntillAuctionEnds(duration);
+        }, 1000);
+        return () => clearInterval(clockInterval);
+    }, [auction]);
+
+    useEffect(() => {
+        socket.on("auction-change", () => {
+            if (!auction) return;
+            getAuction(auction._id).then((res) => setAuction(res.data));
+        });
+    }, [socket]);
+
+    const openModal = () => {
+        if (!user.userID) return ErrorToast("You have to be logged in");
+        setBidModalVisibility(true);
+    };
+
+    const placeBid = async (data: BidOffer) => {
+        if (!user.userID) return ErrorToast("You have to be logged in");
+        if (!auction) return;
+        const placeBidToast = LoadingToast("Placing bid...");
+        if (lastBid && data.offer <= lastBid.offer)
+            return UpdateToast(placeBidToast, "Offer has to be higher than last bid", "error");
+        if (user.userID === auction?.profileID.userID)
+            return UpdateToast(placeBidToast, "You cannot bid your own auction", "error");
+        if (data.profileID == lastBid?.profileID._id)
+            return UpdateToast(placeBidToast, "You cannot bid twice", "error");
+        try {
+            await addBid(data, auction?._id);
+            UpdateToast(placeBidToast, "Successfully placed bid!", "success");
+        } catch (err) {
+            UpdateToast(placeBidToast, "Something gone wrong!", "error");
+        }
+    };
+
     return (
         <section className={style.creatorNetwork}>
             <div className={style.sectionContainer}>
-                <img src={homeImage} alt="Creator Network" className={style.mainImage} />
-                <div>
-                    <h2>the creator network&#174;</h2>
-                    <div className={style.container}>
-                        <ProfileHorizontal
-                            upperText="Enrico"
-                            bottomText="Creator"
-                            imageWidth="50px"
-                            imageUrl={profilePicture}
-                        />
-                        <ProfileHorizontal
-                            upperText="Instant price"
-                            bottomText="3.5 ETH"
-                            imageWidth="50px"
-                            imageUrl={exampleImage}
-                        />
-                    </div>
-                    <div className={style.currentBid}>
-                        <p className={style.currentBidTitle}>Current Bid</p>
-                        <p className={style.ethValue}>1.00 ETH</p>
-                        <p className={style.dolarValue}>$3,618.36</p>
-                        <p className={style.endTime}>Auction ending in</p>
-                        <div className={style.countdown}>
-                            <div>
-                                <p className={style.timeValue}>19</p>
-                                <p className={style.timeLabel}>Hrs</p>
-                            </div>
-                            <div>
-                                <p className={style.timeValue}>24</p>
-                                <p className={style.timeLabel}>mins</p>
-                            </div>
-                            <div>
-                                <p className={style.timeValue}>19</p>
-                                <p className={style.timeLabel}>secs</p>
+                <img
+                    src={auction?.productID.productImageUrl}
+                    alt="Creator Network"
+                    className={style.mainImage}
+                />
+                {auction && (
+                    <div>
+                        <h2>the creator network&#174;</h2>
+                        <div className={style.container}>
+                            <ProfileHorizontal
+                                upperText={auction.profileID.profileName}
+                                bottomText="Creator"
+                                imageWidth="50px"
+                                imageUrl={auction.profileID.profilePicture}
+                                linkTo={`/profile/${auction.profileID._id}`}
+                            />
+                            <ProfileHorizontal
+                                upperText="Instant price"
+                                bottomText={`${auction.price}$`}
+                                imageWidth="50px"
+                                imageUrl={exampleImage}
+                            />
+                        </div>
+                        <div className={style.currentBid}>
+                            <p className={style.currentBidTitle}>Current Bid</p>
+                            {lastBid ? (
+                                <p className={style.ethValue}>{lastBid.offer}$</p>
+                            ) : (
+                                <p className={style.noBids}>Currently no bids</p>
+                            )}
+                            <p className={style.endTime}>Auction ending in</p>
+                            <div className={style.countdown}>
+                                <div>
+                                    <p className={style.timeValue}>
+                                        {timeUntillAuctionEnds?.hours || "0"}
+                                    </p>
+                                    <p className={style.timeLabel}>Hrs</p>
+                                </div>
+                                <div>
+                                    <p className={style.timeValue}>
+                                        {timeUntillAuctionEnds?.minutes || "0"}
+                                    </p>
+                                    <p className={style.timeLabel}>mins</p>
+                                </div>
+                                <div>
+                                    <p className={style.timeValue}>
+                                        {timeUntillAuctionEnds?.seconds || "0"}
+                                    </p>
+                                    <p className={style.timeLabel}>secs</p>
+                                </div>
                             </div>
                         </div>
+                        <div className={style.buttons}>
+                            <button className={style.btnBid} onClick={openModal}>
+                                Place a bid
+                            </button>
+                            <Link to={`auction/${auction._id}`}>
+                                <button className={style.btnView}>View item</button>
+                            </Link>
+                        </div>
+                        <div className={style.arrowButtons}>
+                            <button className={style.arrowButton}>
+                                <img src={arrowLeft} alt="left arrow" />
+                            </button>
+                            <button className={style.arrowButton}>
+                                <img src={arrowRight} alt="right arrow" />
+                            </button>
+                        </div>
                     </div>
-                    <div className={style.buttons}>
-                        <button className={style.btnBid}>Place a bid</button>
-                        <button className={style.btnView}>View item</button>
-                    </div>
-                    <div className={style.arrowButtons}>
-                        <button className={style.arrowButton}>
-                            <img src={arrowLeft} alt="left arrow" />
-                        </button>
-                        <button className={style.arrowButton}>
-                            <img src={arrowRight} alt="right arrow" />
-                        </button>
-                    </div>
-                </div>
+                )}
             </div>
+            <Modal
+                visible={bidModalVisibility}
+                title="Place bid"
+                onClose={() => setBidModalVisibility(false)}
+            >
+                <AddBidModal onPlaceBid={placeBid} onClose={() => setBidModalVisibility(false)} />
+            </Modal>
         </section>
     );
 };
